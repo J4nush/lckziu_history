019a8326a27ac01aec4e06a042f8b86d07674fea
J4nush <szeryfchudy666@gmail.com>
2023-09-29 15:52:05 +0200

    created circual cards

---
+++ src/components/CircleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+<div :class="small ? 'h-[25vh]' : 'h-full'" class="aspect-square rounded-full circual-card flex items-center justify-center
+ text-center"
+    :style="'background-image: linear-gradient( rgba(30, 115, 190, .5), rgba(30, 115, 190, .5)), url('+image+')'">
+    <span class="max-w-[80%] text-white ">{{schoolName}}</span></div>
+</template>
+
+<script setup lang="ts">
+import {defineProps} from "vue"
+const props = defineProps<{
+  image: string,
+  schoolName: string,
+  small: boolean
+}>()
+
+</script>
+
+<style scoped>
+.circual-card{
+  background-size: cover;
+  transition: .3s ease-in;
+}
+.circual-card:hover{
+  filter: brightness(50%);
+  transition: .3s ease-out;
+  box-shadow: 0 0 3em rgb(30, 115, 190);
+}
+</style>
--- src/components/StartComponent.vue
+++ src/components/StartComponent.vue
@@ -6,6 +6,7 @@ import { ref } from 'vue'
 import MenuComponent from '../components/MenuComponent.vue'
 import globalState from "@/globalState";
 import LoadingScreen from "@/components/LoadingScreen.vue";
+import CircleComponent from "@/components/CircleComponent.vue";
 
 const router = useRouter()
 const goToMagnolka = () => {
@@ -22,51 +23,49 @@ const goToMagnolka = () => {
 
     <MenuComponent :items="[]"/>
         <main>
-            <div class="wrapper flex flex-col w-100 p-4">
-                <div class="small_blocks flex flex-row gap-x-40 justify-center w-100 pl-5 pr-5">
-                    <Card class="text-center ">
-                        <template #header>
-                            <img alt="user header" class="small_header_image" src="../assets/dlugosza_thumb.jpg" />
-                        </template>
-                        <template #title >Zespół Szkół przy ulicy Długosza</template>
-                        
-                    </Card>
-                    <Card class=" text-center">
-                        <template #header>
-                            <img alt="user header" class="small_header_image" src="../assets/zdpt_thumb.jpg" />
-                        </template>
-                        <template #title> Zespół Szkół Pedagogicznych i Technicznych </template>
-                        
-                    </Card>
-                    <Card class=" text-center">
-                        <template #header>
-                            <img alt="user header" class="small_header_image" src="../assets/zdpt_thumb.jpg" />
-                        </template>
-                        <template #title>Zespół Szkół jeszcze nie wiadomo jakich</template>
-                        
-                    </Card>
+            <div class="wrapper flex flex-col gap-2 justify-center w-100 p-4">
+                <div class="small_blocks max-h-[25vh] w-[80%] m-auto flex flex-row justify-between w-100 pl-5 pr-5">
+                  <CircleComponent :small="true" schoolName="Zespół Szkół przy ulicy Długosza" :image="`src/assets/dlugosza_thumb.jpg`"/>
+                  <CircleComponent :small="true" schoolName="Zespół Szkół Pedagogicznych i Technicznych" :image="`src/assets/zdpt_thumb.jpg`"/>
+                  <CircleComponent :small="true" schoolName="Zespół Szkół inny gdzieś w LBN" :image="`src/assets/dlugosza_thumb.jpg`"/>
+<!--                    <Card class="text-center ">-->
+<!--                        <template #header>-->
+<!--                            <img alt="user header" class="small_header_image" src="../assets/dlugosza_thumb.jpg" />-->
+<!--                        </template>-->
+<!--                        <template #title >Zespół Szkół przy ulicy Długosza</template>-->
+<!--                        -->
+<!--                    </Card>-->
+<!--                    <Card class=" text-center">-->
+<!--                        <template #header>-->
+<!--                            <img alt="user header" class="small_header_image" src="../assets/zdpt_thumb.jpg" />-->
+<!--                        </template>-->
+<!--                        <template #title> Zespół Szkół Pedagogicznych i Technicznych </template>-->
+<!--                        -->
+<!--                    </Card>-->
+<!--                    <Card class=" text-center">-->
+<!--                        <template #header>-->
+<!--                            <img alt="user header" class="small_header_image" src="../assets/zdpt_thumb.jpg" />-->
+<!--                        </template>-->
+<!--                        <template #title>Zespół Szkół jeszcze nie wiadomo jakich</template>-->
+<!--                        -->
+<!--                    </Card>-->
                 </div>
                 <Divider/>
-                <div class="big_block flex flex-row justify-center w-100">
-                    
-                    <Card class="basis-1/2 text-center p-4 cursor-pointer rounded-full" @click="goToMagnolka">
-                        <template #header>
-                            <img alt="user header" class="big_header_image" src="../assets/lckziu_thumb.jpg" />
-                        </template>
-                        <template #title>Szkoła na ul. Magnoliowej 8</template>
-                    </Card>
+                <div class="big_block flex flex-row justify-center w-100 h-[50vh]">
+                    <CircleComponent :small="false" :image="`src/assets/lckziu_thumb.jpg`" school-name="Szkoła na ul. Magnoliowej 8" @click="goToMagnolka"/>
+<!--                    <Card class="basis-1/2 text-center p-4 cursor-pointer rounded-full" @click="goToMagnolka">-->
+<!--                        <template #header>-->
+<!--                            <img alt="user header" class="big_header_image" src="../assets/lckziu_thumb.jpg" />-->
+<!--                        </template>-->
+<!--                        <template #title>Szkoła na ul. Magnoliowej 8</template>-->
+<!--                    </Card>-->
                 </div>
             </div>
         </main>
 </template>
 <style >
 
-.small_header_image{
-    height: 20vh;
-    aspect-ratio: auto;
-    margin-left: auto;
-    margin-right: auto;
-}
+
 .big_header_image{
     aspect-ratio: auto;
     margin-left: auto;
